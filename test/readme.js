74a0afeadf48d90ca5316a5e9b12e37748f86229
Alan Gutierrez <alan@prettyrobots.com>
2020-12-25 23:14:59 -0600

    More thoughts on async caching.

--- test/readme.js
+++ test/readme.js
@@ -6,7 +6,9 @@
 async function evict (cache, expire) {
     for (;;) {
         const entry = cache.least()
-        if (entry == null || entry.value.when > expire) {
+        if (entry == null) {
+            break
+        } else if (entry.value.when > expire) {
             entry.release()
             break
         }
@@ -24,7 +26,7 @@ async function get (cache, path) {
         let entry = cache.hold(path)
         if (entry == null) {
             const data = await fs.readFile(path)
-            entry = cache.hold({ data, promise: null ])
+            entry = cache.hold(path, { data, promise: null ])
         }
         if (entry.promise == null) {
             return entry
@@ -32,4 +34,82 @@ async function get (cache, path) {
         entry.release()
         await entry.value.promise
     }
+}
+//
+
+// Even more annoying, the data is something that has a close step if the get
+// loses a race condition.
+
+// Also, where do these errors go?
+
+async function evict (cache, size) {
+    while (cache.size > size) {
+        const entry = cache.least()
+        if (entry == null) {
+            break
+        }
+        let latch
+        entry.value.promise = new Promise(resolve => latch = { resolve })
+        try {
+            await entry.value.handle.close()
+            entry.remove()
+        } catch (error) {
+            entry.release()
+            throw error
+        } finally {
+            latch.resolve.call(null)
+            entry.value.promise = null
+        }
+    }
+}
+
+//
+async function get (cache, path) {
+    for (;;) {
+        const entry = cache.hold(path)
+        if (entry == null) {
+            const handle = await fs.open(path, 'a')
+            const entry = cache.hold(path, null)
+            if (entry.value == null) {
+                entry.value = { handle, promise: null }
+            } else {
+                await handle.close()
+            }
+            return entry
+        } else if (entry.promise == null) {
+            return entry
+        } else {
+            entry.release()
+            await entry.value.promise
+        }
+    }
+}
+
+// Not as big of a deal as all that for this appending case.
+async function evict (cache, size) {
+    while (cache.size > size) {
+        const entry = cache.least()
+        if (entry == null) {
+            break
+        }
+        entry.remove()
+        await entry.value.handle.sync()
+        await entry.value.handle.close()
+    }
+}
+
+//
+async function get (cache, path) {
+    const entry = cache.hold(path)
+    if (entry == null) {
+        const handle = await fs.open(path, 'a')
+        const entry = cache.hold(path, null)
+        if (entry.value == null) {
+            entry.value = { handle, promise: null }
+        } else {
+            await handle.close()
+        }
+        return entry
+    }
+    return entry
 }
